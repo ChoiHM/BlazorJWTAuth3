12dafa22265f7c07f5213ad7971b7aa85b5a6239
H.M Choi <choise1825@gmail.com>
2023-09-20 14:35:22 +0900

    초기 커밋.

---
+++ BlazorJWTAuth3/DTO/UserRegister.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorJWTAuth3.DTO
+{
+    public class UserRegister
+    {
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string ConfirmPassword { get; set; } = string.Empty;
+    }
+}
